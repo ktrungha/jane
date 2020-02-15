8c9675ceaec35afae7828c50149371cfae56e702
Trung Ha <ktrungha@hotmail.com>
2020-02-15 11:04:33 -0500

    init commit

---
+++ precache-manifest.36374085ffc5a0a594a293ebd25de006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758837004bf7a68958a26917da2e161e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e276d9fd30e5945f82e6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74ef14b456f9b3dc05e9",
+    "url": "/static/js/2.a115c6f2.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.a115c6f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e276d9fd30e5945f82e6",
+    "url": "/static/js/main.d1cf3692.chunk.js"
+  },
+  {
+    "revision": "183bdabdec7dc54d749f",
+    "url": "/static/js/runtime-main.544078fa.js"
+  }
+]);
